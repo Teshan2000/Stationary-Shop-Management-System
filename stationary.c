86516bf0c00b33f8662de0816fb1a78a91e39344
Teshan2000 <teshan_wijewardhana@outlook.com>
2023-05-09 14:28:40 +0530

    Main function
    Main function of the system is created

--- stationary.c
+++ stationary.c
@@ -1,3 +1,17 @@
 #include <stdio.h>
 
-int main() {}
+int main() {
+    int choice;
+    printf("Welcome to our Boookshop!\n");
+    printf("Log as a customer");
+    scanf("%d", &choice);
+}
+
+void customer() {
+    int item;
+    printf("Choose your stationary item");
+    printf("Books");
+    printf("Pencils");
+    printf("Pens");
+    scanf("%d", &item);
+}
